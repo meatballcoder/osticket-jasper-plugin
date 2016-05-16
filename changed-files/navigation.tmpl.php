334dfabf71c2fa8e5697043bed32227b8041f8b8
meatballcoder <good_comments@hotmail.com>
2016-05-16 18:18:08 -0400

    Create navigation.tmpl.php
    This is where I put $ost->isPlugin().  This is subject to change, if I find a better way to fix relative links.

---
+++ changed-files/navigation.tmpl.php
@@ -0,0 +1,80 @@
+<?php
+//ORG
+// if(($tabs=$nav->getTabs()) && is_array($tabs)){
+    // foreach($tabs as $name =>$tab) {
+        // echo sprintf('<li class="%s %s"><a href="%s">%s</a>',
+            // $tab['active'] ? 'active':'inactive',
+            // @$tab['class'] ?: '',
+            // $tab['href'],$tab['desc']);
+        // if(!$tab['active'] && ($subnav=$nav->getSubMenu($name))){
+            // echo "<ul>\n";
+            // foreach($subnav as $k => $item) {
+                // if (!($id=$item['id']))
+                    // $id="nav$k";
+
+                // echo sprintf(
+                    // '<li><a class="%s" href="%s" title="%s" id="%s">%s</a></li>',
+                    // $item['iconclass'],
+                    // $item['href'], $item['title'],
+                    // $id, $item['desc']);
+            // }
+            // echo "\n</ul>\n";
+        // }
+        // echo "\n</li>\n";
+    // }
+// }
+
+//Mine
+if(($tabs=$nav->getTabs()) && is_array($tabs)){
+
+    foreach($tabs as $name =>$tab) {
+		echo sprintf('<li class="%s %s"><a href="'.  $ost->isPlugin()    .'%s">%s</a>',
+            $tab['active'] ? 'active':'inactive',
+            @$tab['class'] ?: '',
+            $tab['href'],$tab['desc']);
+        if(!$tab['active'] && ($subnav=$nav->getSubMenu($name))){
+            echo "<ul>\n";
+            foreach($subnav as $k => $item) {
+                if (!($id=$item['id']))
+                    $id="nav$k";
+
+                echo sprintf(
+                    '<li><a class="%s" href="'.  $ost->isPlugin()    .'%s" title="%s" id="%s">%s</a></li>',
+                    $item['iconclass'],
+                    $item['href'], $item['title'],
+                    $id, $item['desc']);
+            }
+            echo "\n</ul>\n";
+        }
+        echo "\n</li>\n";
+    }
+} 
+
+//From the current develop branch of osTicket
+// if(($tabs=$nav->getTabs()) && is_array($tabs)){
+    // foreach($tabs as $name =>$tab) {
+        // if ($tab['href'][0] != '/')
+            // $tab['href'] = ROOT_PATH . 'scp/' . $tab['href'];
+        // echo sprintf('<li class="%s %s"><a href="%s">%s</a>',
+            // $tab['active'] ? 'active':'inactive',
+            // @$tab['class'] ?: '',
+            // $tab['href'],$tab['desc']);
+        // if(!$tab['active'] && ($subnav=$nav->getSubMenu($name))){
+            // echo "<ul>\n";
+            // foreach($subnav as $k => $item) {
+                // if (!($id=$item['id']))
+                    // $id="nav$k";
+                // if ($item['href'][0] != '/')
+                    // $item['href'] = ROOT_PATH . 'scp/' . $item['href'];
+                // echo sprintf(
+                    // '<li><a class="%s" href="%s" title="%s" id="%s">%s</a></li>',
+                    // $item['iconclass'],
+                    // $item['href'], $item['title'],
+                    // $id, $item['desc']);
+            // }
+            // echo "\n</ul>\n";
+        // }
+        // echo "\n</li>\n";
+    // }
+// } 
+?>
