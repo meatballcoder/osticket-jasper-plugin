88b94a526cfb3a5be2ac1e9435dc15d5cba5a32d
meatballcoder <good_comments@hotmail.com>
2016-05-16 18:12:55 -0400

    Add files via upload

---
+++ jasper-reports/include/controller/JasperMasterController.php
@@ -0,0 +1,17 @@
+<?PHP
+namespace controller;
+
+/**
+ * Description of Controller
+ *
+ * @author meatballcoder
+ */
+require_once(INCLUDE_DIR . 'class.staff.php');
+
+class JasperMasterController {
+	function __construct(){
+		global $ost, $msg, $cfg;
+	}
+
+}
+?>
