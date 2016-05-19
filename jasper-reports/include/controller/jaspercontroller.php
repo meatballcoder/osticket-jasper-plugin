c6649ad0eb1df96bd0814ad2cf423b98e450e2df
meatballcoder <good_comments@hotmail.com>
2016-05-19 14:48:57 -0400

    Add files via upload
    Added function to get report list for dropdownlist.  Added extra POST variables to stats.

--- jasper-reports/include/controller/jaspercontroller.php
+++ jasper-reports/include/controller/jaspercontroller.php
@@ -15,18 +15,20 @@ class JasperController extends JasperMasterController {
 	private $c;
 	
 	function __construct() {
-		
+		global $ost, $msg, $cfg; //I had to put these in all the functions
 		$this->c = new Client(
 						"http://localhost:8080/jasperserver",
 						"jasperadmin",
 						"jasperadmin"
 					);
 		$this->renderer = new JasperRenderView();
-
     }
 	public function searchAction(){
 		global $ost, $msg, $cfg; //I had to put these in all the functions
 		$ost->setPluginInstance(OST_WEB_ROOT.'scp/');
+		
+		$reports_available = $this->getReportsAvailable();
+		
 		require('staff.inc.php');
 		include(STAFFINC_DIR.'header.inc.php');
 		require(JASPER_REPORTS_VIEWS.'osticket-reports.html');
@@ -34,7 +36,7 @@ class JasperController extends JasperMasterController {
 
 	public function getTicketStats(){
 		
-		
+		global $ost, $msg, $cfg; //I had to put these in all the functions
 		$ost->setPluginInstance(OST_WEB_ROOT.'scp/');  //based upon my changing the $ost class
 
 		$report_format=$_POST['report_format'];
@@ -45,6 +47,7 @@ class JasperController extends JasperMasterController {
 
 		$start_date=$_POST['start_date'];
 		$end_date=$_POST['end_date'];
+		$report_path = $_POST['report_name'];
 
 		$js = $this->c->jobService();               
 		$this->c->setRequestTimeout(60); 
@@ -55,8 +58,19 @@ class JasperController extends JasperMasterController {
 		   'ticketEndDate' => array($end_date)
 		);
 
-		$report = $this->c->reportService()->runReport('/reports/osTicket/HelpTopics', $report_format, null, null, $controls);
+		$report = $this->c->reportService()->runReport($report_path, $report_format, null, null, $controls);
 		$this->renderer->render_view($report, $report_format);
+	}
+	public function getReportsAvailable(){
+		
+		// Search for specific items in repository
+		$criteria = new RepositorySearchCriteria();
+		$criteria->folderUri="/reports/osTicket";
+		//$criteria->q = "";
+		 
+		//it brings back objects, so no need to convert any json;
+		return $this->c->repositoryService()->searchResources($criteria);
+	 	
 	}
 }
 ?>
