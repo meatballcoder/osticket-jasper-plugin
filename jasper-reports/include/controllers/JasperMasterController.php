5864eb7924c76c7c14a2545d2b1f2b8be5b34617
meatballcoder <good_comments@hotmail.com>
2016-05-19 15:53:47 -0400

    Create JasperMasterController.php
    Changed directory name to plural, controllers.

---
+++ jasper-reports/include/controllers/JasperMasterController.php
@@ -0,0 +1,18 @@
+<?PHP
+namespace controllers;
+/**
+ * Description of Controller
+ *
+ * @author meatballcoder
+ */
+require_once(INCLUDE_DIR . 'class.staff.php');
+
+
+class JasperMasterController {
+	
+	function __construct(){
+		global $ost, $msg, $cfg;
+	}
+
+}
+?>
